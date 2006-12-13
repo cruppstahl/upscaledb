52b0e3984c74296c354582403a1cba2718c8c07b
cruppstahl <chris@crupp.de>
2006-12-13 19:07:25 +0000

    new valgrind tests
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@90 66673985-dd14-0410-9433-caba4d15c716

---
+++ tests/valgrind.sh
@@ -0,0 +1,27 @@
+
+dir=../../../hamsterdb-tests/trunk/testfiles
+
+files="1/45.tst 1/220.tst 1/ext_020.tst 1/ext_060.tst 1/blb-001.tst"
+
+function call {
+    for file in $files
+    do
+        echo "running $dir/$file $1 $2 $3 $4 $5"
+        valgrind --tool=memcheck ./test --db $dir/$file $1 $2 $3 $4 $5 
+    done
+}
+
+call "--reopen=1" 
+call "--overwrite=1 --reopen=1" 
+call "--inmemorydb=1" 
+call "--overwrite=1 --inmemorydb=1" 
+call "--mmap=0 --overwrite=1 --reopen=1" 
+call "--keysize=8 --overwrite=1 --reopen=1" 
+call "--keysize=12 --overwrite=1 --reopen=1" 
+call "--keysize=33 --overwrite=1 --reopen=1" 
+call "--keysize=680 --overwrite=1 --reopen=1" 
+call "--pagesize=1024 --overwrite=1 --reopen=1" 
+call "--pagesize=3072 --overwrite=1 --reopen=1" 
+call "--pagesize=8192 --overwrite=1 --reopen=1" 
+call "--cachesize=0 --overwrite=1 --reopen=1" 
+call "--cachepolicy=strict --overwrite=1 --reopen=1" 
