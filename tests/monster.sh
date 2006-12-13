fdb4c1a613f57333e14d9cf5fbc81acbc93ed0d4
cruppstahl <chris@crupp.de>
2006-12-13 19:02:14 +0000

    new test parameter --optimizesize
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@86 66673985-dd14-0410-9433-caba4d15c716

--- tests/monster.sh
+++ tests/monster.sh
@@ -4,33 +4,46 @@ d=1
 ./run-tests.sh $d --reopen=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --overwrite=1 --reopen=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --inmemorydb=1 $1 $2 $3 $4 $5
-./run-tests.sh $d  --overwrite=1 --inmemorydb=1 $1 $2 $3 $4 $5
+./run-tests.sh $d --overwrite=1 --inmemorydb=1 $1 $2 $3 $4 $5
+
+./run-tests.sh $d --optimizesize=1 --reopen=1 $1 $2 $3 $4 $5
+./run-tests.sh $d --optimizesize=1 --overwrite=1 --reopen=1 $1 $2 $3 $4 $5
+./run-tests.sh $d --optimizesize=1 --inmemorydb=1 $1 $2 $3 $4 $5
+./run-tests.sh $d --optimizesize=1 --overwrite=1 --inmemorydb=1 $1 $2 $3 $4 $5
 
 ./run-tests.sh $d --mmap=0 --reopen=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --mmap=0 --overwrite=1 --reopen=1 $1 $2 $3 $4 $5
 
+./run-tests.sh $d --optimizesize=1 --mmap=0 --reopen=1 $1 $2 $3 $4 $5
+./run-tests.sh $d --optimizesize=1 --mmap=0 --overwrite=1 --reopen=1 $1 $2 $3 $4 $5
+
 ./run-tests.sh $d --keysize=8 --reopen=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=8 --overwrite=1 --reopen=1 $1 $2 $3 $4 $5
+./run-tests.sh $d --keysize=8 --overwrite=1 --reopen=1 --optimizesize=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=8 --inmemorydb=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=8 --overwrite=1 --inmemorydb=1 $1 $2 $3 $4 $5
 
 ./run-tests.sh $d --keysize=12 --reopen=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=12 --overwrite=1 --reopen=1 $1 $2 $3 $4 $5
+./run-tests.sh $d --keysize=12 --overwrite=1 --reopen=1 --optimizesize=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=12 --inmemorydb=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=12 --overwrite=1 --inmemorydb=1 $1 $2 $3 $4 $5
 
 ./run-tests.sh $d --keysize=33 --reopen=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=33 --overwrite=1 --reopen=1 $1 $2 $3 $4 $5
+./run-tests.sh $d --keysize=33 --overwrite=1 --reopen=1 --optimizesize=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=33 --inmemorydb=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=33 --overwrite=1 --inmemorydb=1 $1 $2 $3 $4 $5
 
 ./run-tests.sh $d --keysize=680 --reopen=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=680 --overwrite=1 --reopen=1 $1 $2 $3 $4 $5
+./run-tests.sh $d --keysize=680 --overwrite=1 --reopen=1 --optimizesize=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=680 --inmemorydb=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --keysize=680 --overwrite=1 --inmemorydb=1 $1 $2 $3 $4 $5
 
 ./run-tests.sh $d --pagesize=1024 --reopen=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --pagesize=1024 --overwrite=1 --reopen=1 $1 $2 $3 $4 $5
+./run-tests.sh $d --pagesize=1024 --overwrite=1 --reopen=1 --optimizesize=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --pagesize=1024 --inmemorydb=1 $1 $2 $3 $4 $5
 ./run-tests.sh $d --pagesize=1024 --overwrite=1 --inmemorydb=1 $1 $2 $3 $4 $5
 
