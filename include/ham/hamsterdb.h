a643ef7e4da1853e5c6fb33acec11e43d62c3bcc
cruppstahl <chris@crupp.de>
2006-12-28 10:35:24 +0000

    changed cursor interface, added move-function
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@115 66673985-dd14-0410-9433-caba4d15c716

--- include/ham/hamsterdb.h
+++ include/ham/hamsterdb.h
@@ -11,14 +11,14 @@
 
 #ifdef __cplusplus
 extern "C" {
-#endif 
+#endif
 
 #include <ham/types.h>
 
 /**
  * the database structure
  */
-struct ham_db_t; 
+struct ham_db_t;
 typedef struct ham_db_t ham_db_t;
 
 /**
@@ -41,7 +41,7 @@ typedef struct
 } ham_record_t;
 
 /**
- * flags for ham_record_t: data points to memory which is allocated 
+ * flags for ham_record_t: data points to memory which is allocated
  * by the user
  */
 #define HAM_RECORD_USER_ALLOC   1
@@ -58,7 +58,7 @@ typedef struct
 } ham_key_t;
 
 /**
- * flags for ham_key_t: data points to memory which is allocated 
+ * flags for ham_key_t: data points to memory which is allocated
  * by the user. this is only useful if you read the key from a cursor
  * (ham_cursor_get_key());
  */
@@ -70,14 +70,14 @@ typedef struct
 typedef void (*ham_errhandler_fun)(const char *message);
 
 /**
- * set a global error handler; this handler will receive <b>all</b> debug 
+ * set a global error handler; this handler will receive <b>all</b> debug
  * messages which are emitted by hamster-db. you can remove the handler by
  * setting @a f to 0.
  */
 extern void
 ham_set_errhandler(ham_errhandler_fun f);
 
-/** 
+/**
  * get a descriptive error string from a hamster status code
  */
 extern const char *
@@ -85,7 +85,7 @@ ham_strerror(ham_status_t result);
 
 /**
  * hamster error- and status codes.
- * hamster-error codes are always negative, so we have no conflicts with 
+ * hamster-error codes are always negative, so we have no conflicts with
  * errno.h
  */
 #define HAM_SUCCESS                  (  0)
@@ -115,7 +115,7 @@ ham_strerror(ham_status_t result);
  * get the version of the hamsterdb library
  */
 extern void
-ham_get_version(ham_u32_t *major, ham_u32_t *minor, 
+ham_get_version(ham_u32_t *major, ham_u32_t *minor,
         ham_u32_t *revision);
 
 /**
@@ -140,27 +140,27 @@ ham_open(ham_db_t *db, const char *filename, ham_u32_t flags);
  * open an (existing) database - extended version
  */
 extern ham_status_t
-ham_open_ex(ham_db_t *db, const char *filename, 
+ham_open_ex(ham_db_t *db, const char *filename,
         ham_u32_t flags, ham_size_t cachesize);
 
 /**
  * create a new database
  */
 extern ham_status_t
-ham_create(ham_db_t *db, const char *filename, 
+ham_create(ham_db_t *db, const char *filename,
         ham_u32_t flags, ham_u32_t mode);
 
 /**
  * create a new database - extended version
  *
  * !!
- * If you create an in-memory-database (flag HAM_IN_MEMORY_DB), 
- * you are NOT allowed to set the flag HAM_CACHE_STRICT or to use 
- * a cache size != 0! 
+ * If you create an in-memory-database (flag HAM_IN_MEMORY_DB),
+ * you are NOT allowed to set the flag HAM_CACHE_STRICT or to use
+ * a cache size != 0!
  */
 extern ham_status_t
-ham_create_ex(ham_db_t *db, const char *filename, 
-        ham_u32_t flags, ham_u32_t mode, ham_u16_t pagesize, 
+ham_create_ex(ham_db_t *db, const char *filename,
+        ham_u32_t flags, ham_u32_t mode, ham_u16_t pagesize,
         ham_u16_t keysize, ham_size_t cachesize);
 
 /**
@@ -172,7 +172,7 @@ ham_create_ex(ham_db_t *db, const char *filename,
 #define HAM_WRITE_THROUGH            0x00000001
 
 /**
- * If ham_open() fails, if a file with the given name does not 
+ * If ham_open() fails, if a file with the given name does not
  * exist. If you set the flag HAM_OPEN_CREATE, the file will
  * be created if it does not exist.
  * The flag is disabled by default.
@@ -211,7 +211,7 @@ ham_create_ex(ham_db_t *db, const char *filename,
 #define HAM_DISABLE_VAR_KEYLEN       0x00000040
 
 /**
- * create an in-memory-database. No file will be written, and the 
+ * create an in-memory-database. No file will be written, and the
  * database contents are lost after the database is closed.
  * This flag is disabled by default.
  */
@@ -231,9 +231,9 @@ ham_create_ex(ham_db_t *db, const char *filename,
 /**
  * Flags and policies for the database cache
  *
- * If this flag is true, the cache must *never* be bigger then 
- * the maximum cachesize; otherwise, the cache is allowed to allocate 
- * more pages then the maximum cachesize, but only if it's necessary and 
+ * If this flag is true, the cache must *never* be bigger then
+ * the maximum cachesize; otherwise, the cache is allowed to allocate
+ * more pages then the maximum cachesize, but only if it's necessary and
  * only for a short time. Default: flag is off
  *
  * !!
@@ -251,8 +251,8 @@ ham_create_ex(ham_db_t *db, const char *filename,
 
 /**
  * Optimize for smaller database files; this will try to merge freelist
- * entries, whenever possible. Files can become significantly smaller, 
- * but it costs performance, especially when ham_erase() is called 
+ * entries, whenever possible. Files can become significantly smaller,
+ * but it costs performance, especially when ham_erase() is called
  * frequently. Default: flag is off.
  */
 #define HAM_OPTIMIZE_SIZE            0x00001000
@@ -263,16 +263,16 @@ ham_create_ex(ham_db_t *db, const char *filename,
  * @remark set reserved and flags to 0
  */
 extern ham_status_t
-ham_create_cursor(ham_db_t *db, void *reserved, ham_u32_t flags, 
+ham_create_cursor(ham_db_t *db, void *reserved, ham_u32_t flags,
             ham_cursor_t **cursor);
 
-/** 
+/**
  * get the last error code
  */
 extern ham_status_t
 ham_get_error(ham_db_t *db);
 
-/** 
+/**
  * set the prefix comparison function
  *
  * @remark the prefix comparison function is called when an index uses
@@ -282,7 +282,7 @@ ham_get_error(ham_db_t *db);
 extern ham_status_t
 ham_set_prefix_compare_func(ham_db_t *db, ham_prefix_compare_func_t foo);
 
-/** 
+/**
  * set the default comparison function
  *
  * @remark the default comparison function is called when an index does NOT
@@ -291,16 +291,16 @@ ham_set_prefix_compare_func(ham_db_t *db, ham_prefix_compare_func_t foo);
 extern ham_status_t
 ham_set_compare_func(ham_db_t *db, ham_compare_func_t foo);
 
-/** 
+/**
  * find a key in the database
  *
  * @remark set 'reserved' to NULL
  */
 extern ham_status_t
-ham_find(ham_db_t *db, void *reserved, ham_key_t *key, 
+ham_find(ham_db_t *db, void *reserved, ham_key_t *key,
         ham_record_t *record, ham_u32_t flags);
 
-/** 
+/**
  * insert a database entry
  *
  * @remark set 'reserved' to NULL
@@ -308,19 +308,19 @@ ham_find(ham_db_t *db, void *reserved, ham_key_t *key,
  * @remark see below for valid flags
  */
 extern ham_status_t
-ham_insert(ham_db_t *db, void *reserved, ham_key_t *key, 
+ham_insert(ham_db_t *db, void *reserved, ham_key_t *key,
         ham_record_t *record, ham_u32_t flags);
 
 /** insert-flag: overwrite the key/record pair, if it exists */
 #define HAM_OVERWRITE               1
 
-/** 
+/**
  * erase a database entry
  *
  * @remark set 'reserved' to NULL
  */
 extern ham_status_t
-ham_erase(ham_db_t *db, void *reserved, ham_key_t *key, 
+ham_erase(ham_db_t *db, void *reserved, ham_key_t *key,
         ham_u32_t flags);
 
 /**
@@ -343,7 +343,7 @@ ham_close(ham_db_t *db);
  * @remark set @a reserved to NULL and @a flags to 0
  */
 extern ham_status_t
-ham_cursor_create(ham_db_t *db, void *reserved, ham_u32_t flags, 
+ham_cursor_create(ham_db_t *db, void *reserved, ham_u32_t flags,
         ham_cursor_t **cursor);
 
 /**
@@ -356,11 +356,12 @@ ham_cursor_clone(ham_cursor_t *src, ham_cursor_t **dest);
  * moves the cursor
  *
  * @remark @a key and/or @a record can be NULL; if @a flag is neither
- * first, last, next or previous, the current key/record is returned
+ * HAM_CURSOR_FIRST, HAM_CURSOR_LAST, HAM_CURSOR_NEXT or HAM_CURSOR_PREVIOUS,
+ * the current key/record is returned
  */
 extern ham_status_t
-ham_cursor_move(ham_cursor_t *cursor, ham_key_t *key, 
-        ham_record_t *record, ham_u32_t flags;
+ham_cursor_move(ham_cursor_t *cursor, ham_key_t *key,
+        ham_record_t *record, ham_u32_t flags);
 
 #define HAM_CURSOR_FIRST            1
 #define HAM_CURSOR_LAST             2
@@ -370,7 +371,7 @@ ham_cursor_move(ham_cursor_t *cursor, ham_key_t *key,
 /**
  * replace the current record
  */
-extern ham_status_t 
+extern ham_status_t
 ham_cursor_replace(ham_cursor_t *cursor, ham_record_t *record,
             ham_u32_t flags);
 
@@ -378,13 +379,13 @@ ham_cursor_replace(ham_cursor_t *cursor, ham_record_t *record,
  * find a key in the index and positions the cursor
  * on this key
  */
-extern ham_status_t 
+extern ham_status_t
 ham_cursor_find(ham_cursor_t *cursor, ham_key_t *key, ham_u32_t flags);
 
 /**
  * insert (or update) a key in the index
  */
-extern ham_status_t 
+extern ham_status_t
 ham_cursor_insert(ham_cursor_t *cursor, ham_key_t *key,
             ham_record_t *record, ham_u32_t flags);
 
@@ -392,7 +393,7 @@ ham_cursor_insert(ham_cursor_t *cursor, ham_key_t *key,
  * erases the key from the index and positions the cursor to the
  * next key
  */
-extern ham_status_t 
+extern ham_status_t
 ham_cursor_erase(ham_cursor_t *cursor, ham_u32_t flags);
 
 /**
@@ -404,6 +405,6 @@ ham_cursor_close(ham_cursor_t *cursor);
 
 #ifdef __cplusplus
 } // extern "C"
-#endif 
+#endif
 
 #endif /* HAM_HAMSTERDB_H__ */
