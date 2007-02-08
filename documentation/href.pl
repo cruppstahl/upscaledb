1fc5180b17715e5239345f836b23611157796c6c
cruppstahl <chris@crupp.de>
2007-02-08 22:43:04 +0000

    make doxygen for online documentation
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@143 66673985-dd14-0410-9433-caba4d15c716

---
+++ documentation/href.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+opendir(DH, "html") or die "cannot open subdir: $!\n";
+@f=readdir(DH);
+closedir(DH);
+foreach (@f) {
+    if (/\.html$/) {
+        process("html/$_");
+    }
+}
+
+sub process
+{
+    my $f=shift;
+    print "$f\n";
+
+    open(TH, ">.tmp") or die "cannot open .tmp: $!\n";
+
+    open(FH, $f) or die "cannot open $f: $!\n";
+    while (<FH>) {
+        s/href=\"(.*?)\"/href=\"?page=doxygen&module=$1\"/g;
+        print TH $_;
+    }
+    close(FH);
+    close(TH);
+
+    `cp .tmp $f`;
+}
---
+++ documentation/make_www.sh
@@ -0,0 +1,6 @@
+cd ..
+doxygen documentation/Doxyfile.www
+cd documentation
+./href.pl
+rm html_www/*.css
+tar -cvf html.tar html_www/*
