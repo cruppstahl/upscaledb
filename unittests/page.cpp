47a861790d20788ed81c7f86a4084e8ee4d54169
cruppstahl <chris@crupp.de>
2007-04-03 09:31:23 +0000

    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@239 66673985-dd14-0410-9433-caba4d15c716

---
+++ unittests/page.cpp
@@ -0,0 +1,89 @@
+/**
+ * Copyright (C) 2005-2007 Christoph Rupp (chris@crupp.de).
+ * All rights reserved. See file LICENSE for licence and copyright
+ * information.
+ *
+ * unit tests for mem.h/mem.c
+ *
+ */
+
+#include <stdexcept>
+#include <cppunit/extensions/HelperMacros.h>
+#include <ham/hamsterdb.h>
+#include "../src/db.h"
+#include "../src/page.h"
+#include "../src/device.h"
+#include "memtracker.h"
+
+class PageTest : public CppUnit::TestFixture
+{
+    CPPUNIT_TEST_SUITE(PageTest);
+    CPPUNIT_TEST      (newDeleteTest);
+    CPPUNIT_TEST      (allocFreeTest);
+    CPPUNIT_TEST      (fetchFlushTest);
+    CPPUNIT_TEST      (negativeFreeTest);
+    CPPUNIT_TEST_SUITE_END();
+
+protected:
+    ham_db_t *m_db;
+    ham_bool_t m_inmemory;
+    ham_device_t *m_dev;
+    memtracker_t *m_alloc;
+
+public:
+    PageTest(ham_bool_t inmemorydb=HAM_FALSE)
+    :   m_inmemory(inmemorydb)
+    {
+    } 
+
+    void setUp()
+    { 
+        m_alloc=memtracker_new();
+        CPPUNIT_ASSERT(0==ham_new(&m_db));
+        db_set_allocator(m_db, (mem_allocator_t *)m_alloc);
+        db_set_device(m_db, (m_dev=ham_device_new(m_db, m_inmemory)));
+        db_set_pagesize(m_db, m_dev->get_pagesize(m_dev));
+    }
+    
+    void tearDown() 
+    { 
+        ham_delete(m_db);
+        CPPUNIT_ASSERT(!memtracker_get_leaks(m_alloc));
+    }
+
+    void newDeleteTest()
+    {
+        ham_page_t *page;
+        page=page_new(m_db);
+        CPPUNIT_ASSERT(page!=0);
+        page_delete(page);
+    }
+
+    void allocFreeTest()
+    {
+        CPPUNIT_ASSERT(!m_dev->is_open(m_dev));
+        CPPUNIT_ASSERT(m_dev->create(m_dev, ".test", 0, 0644)==HAM_SUCCESS);
+        CPPUNIT_ASSERT(m_dev->is_open(m_dev));
+        CPPUNIT_ASSERT(m_dev->close(m_dev)==HAM_SUCCESS);
+        CPPUNIT_ASSERT(!m_dev->is_open(m_dev));
+    }
+
+    void fetchFlushTest()
+    {
+        if (!m_inmemory) {
+            CPPUNIT_ASSERT(!m_dev->is_open(m_dev));
+            CPPUNIT_ASSERT(m_dev->create(m_dev, ".test", 0, 0644)==HAM_SUCCESS);
+            CPPUNIT_ASSERT(m_dev->is_open(m_dev));
+            CPPUNIT_ASSERT(m_dev->close(m_dev)==HAM_SUCCESS);
+            CPPUNIT_ASSERT(!m_dev->is_open(m_dev));
+            CPPUNIT_ASSERT(m_dev->open(m_dev, ".test", 0)==HAM_SUCCESS);
+            CPPUNIT_ASSERT(m_dev->is_open(m_dev));
+            CPPUNIT_ASSERT(m_dev->close(m_dev)==HAM_SUCCESS);
+            CPPUNIT_ASSERT(!m_dev->is_open(m_dev));
+        }
+    }
+
+};
+
+CPPUNIT_TEST_SUITE_REGISTRATION(PageTest);
+
