9c15a260c5ef5eabe771935174c0c34f3a73cca5
cruppstahl <chris@crupp.de>
2007-05-12 23:01:02 +0000

    ham_close: assert that refcount is zero
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@285 66673985-dd14-0410-9433-caba4d15c716

--- src/db.c
+++ src/db.c
@@ -740,16 +740,13 @@ db_flush_all(ham_db_t *db, ham_u32_t flags)
     while (head) {
         ham_page_t *next=page_get_next(head, PAGE_LIST_CACHED);
 
-        /* TODO - only if we're closing, but not if we call ham_flush()
-        ham_assert(page_get_refcount(head)==0, 
-                ("page is in use, but database is closing"));
-                */
-
         /*
          * don't remove the page from the cache, if flag NODELETE
          * is set (this flag is used i.e. in ham_flush())
          */
         if (!(flags&DB_FLUSH_NODELETE)) {
+            ham_assert(page_get_refcount(head)==0, 
+                ("page is in use, but database is closing"));
             cache_set_totallist(db_get_cache(db), 
                 page_list_remove(cache_get_totallist(db_get_cache(db)), 
                 PAGE_LIST_CACHED, head));
