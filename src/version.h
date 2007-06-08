e503fa2996c16b7d171c0f95ee26309bdc64dbc6
cruppstahl <chris@crupp.de>
2007-06-08 16:10:32 +0000

    prepared release 0.4.3
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@336 66673985-dd14-0410-9433-caba4d15c716

--- src/version.h
+++ src/version.h
@@ -25,8 +25,8 @@ extern "C" {
  */
 #define HAM_VERSION_MAJ 0
 #define HAM_VERSION_MIN 4
-#define HAM_VERSION_REV 2
-#define HAM_VERSION_STR "0.4.2"
+#define HAM_VERSION_REV 3
+#define HAM_VERSION_STR "0.4.3"
 
 
 #ifdef __cplusplus
