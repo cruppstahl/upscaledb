077745556d2481e57aab16de8dfa8a3ec66eceb4
cruppstahl <chris@crupp.de>
2007-08-02 20:18:51 +0000

    fix for MSVC
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@517 66673985-dd14-0410-9433-caba4d15c716

--- samples/db4.c
+++ samples/db4.c
@@ -113,7 +113,7 @@ main(int argc, char **argv)
          * print the record number and the word 
          */
 #if WIN32
-        printf("%I64: %s\n", *(ham_u64_t *)key.data, (const char *)record.data);
+        printf("%I64u: %s\n", *(ham_u64_t *)key.data, (const char *)record.data);
 #else
         printf("%llu: %s\n", *(ham_u64_t *)key.data, (const char *)record.data);
 #endif
