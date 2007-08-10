2c4fb5729d8291ea31794135336f0023387bd33f
cruppstahl <chris@crupp.de>
2007-08-10 11:38:29 +0000

    fixed header comments
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@552 66673985-dd14-0410-9433-caba4d15c716

--- src/packstart.h
+++ src/packstart.h
@@ -1,7 +1,13 @@
 /**
  * Copyright (C) 2005-2007 Christoph Rupp (chris@crupp.de).
- * All rights reserved. See file LICENSE for licence and copyright
- * information.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ *
+ * See file COPYING.GPL2 and COPYING.GPL3 for License information.
+ *
  *
  * macros for packing structures; should work with most compilers.
  *
