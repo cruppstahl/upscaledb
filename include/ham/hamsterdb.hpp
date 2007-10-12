7a5aac9586275a1874fbb84210ae9b57cc956057
cruppstahl <chris@crupp.de>
2007-10-12 12:34:37 +0000

    added cursors
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@695 66673985-dd14-0410-9433-caba4d15c716

--- include/ham/hamsterdb.hpp
+++ include/ham/hamsterdb.hpp
@@ -1,28 +1,35 @@
+/**
+ * Copyright (C) 2005-2007 Christoph Rupp (chris@crupp.de).
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ *
+ * See file COPYING.GPL2 and COPYING.GPL3 for License information.
+ *
+ *
+ * \mainpage hamsterdb C++ header file
+ * \file hamsterdb.hpp
+ * \author Christoph Rupp, chris@crupp.de
+ * \version 0.4.7
+ */
+
+#ifndef HAM_HAMSTERDB_HPP__
+#define HAM_HAMSTERDB_HPP__
 
 /*
  * TODO TODO TODO
  *
  * x syntaxfehler raus
+ * x header-kommentar einfuegen!
  *
- * o header-kommentar einfügen!
- *
- * o 100% inline, keine c++-datei!
- *
- * o cursor fehlen komplett
- * o operator[] kommt nicht! rein
- * o cursor::begin(), cursor::end()
- * o cursor::next(), cursor::previous(), ++, -- (wie bei anderen iteratoren)
- *
- * o environments fehlen komplett
- *
- * o typedef cursor iterator;
- * o cursor::operator++, cursor::operator--
+ * x cursor: begin/end/++/--: sind das die iterator-deklarationen??
  *
  * o sicherstellen dass die std::algorithm-funktionen mit den iteratoren
  *      funktionieren
  *
- * o funktionen haben keinen ham_status_t als rückgabewert, sondern void
- *      - fehler werden durch exceptions zurückgegeben
+ * o environments fehlen komplett
  *
  * o 100%ige abdeckung durch unittests!
  *
@@ -30,10 +37,7 @@
  * TODO TODO TODO
  */
 
-#ifndef HAMSTERDB_HPP__
-#define HAMSTERDB_HPP__
-
-#include <ham/hamsterdb.h>
+#include <ham/hamsterdb_int.h>
 #include <string.h>
 
 namespace ham {
@@ -50,7 +54,7 @@ public:
     }
 
     /* get an error string */
-    const char *get_desc() {
+    const char *get_string() {
         return (ham_strerror(m_errno));
     }
 
@@ -173,6 +177,20 @@ protected:
 class db
 {
 public:
+    class cursor;
+    typedef cursor iterator;
+
+    /* set error handler function */
+    static ham_status_t set_errhandler(ham_errhandler_fun f) {
+        return (ham_set_errhandler(f));
+    }
+
+    /* get hamsterdb library version */
+    static void get_version(ham_u32_t *major, ham_u32_t *minor, 
+                    ham_u32_t *revision) {
+        ham_get_version(major, minor, revision);
+    }
+
     /* constructor */
     db() : m_db(0), m_opened(false) { 
     }
@@ -188,36 +206,34 @@ public:
 
     /* create a database */
     void create(const char *filename, ham_u32_t flags=0, 
-            ham_u32_t mode=0644, ham_u16_t pagesize=0, 
-            ham_u16_t keysize=0, ham_size_t cachesize=0) {
+            ham_u32_t mode=0644, ham_parameter_t *param=0) {
         ham_status_t st;
         if (!m_db) {
             st=ham_new(&m_db);
             if (st)
                 throw error(st);
         }
-        if ((st=ham_create_ex(m_db, filename, flags, mode, pagesize, keysize, 
-                cachesize)))
+        if ((st=ham_create_ex(m_db, filename, flags, mode, param)))
             throw error(st);
         m_opened=true;
     }
 
     /* open a database */
     void open(const char *filename, ham_u32_t flags=0,
-            ham_size_t cachesize=0) {
+            ham_parameter_t *param=0) {
         ham_status_t st;
         if (!m_db) {
             st=ham_new(&m_db);
             if (st)
                 throw error(st);
         }
-        if ((st=ham_open_ex(m_db, filename, flags, cachesize)))
+        if ((st=ham_open_ex(m_db, filename, flags, param)))
             throw error(st);
         m_opened=true;
     }
 
     /* get the last database error */
-    ham_status_t get_last_error() { 
+    ham_status_t get_error() { 
         return (ham_get_error(m_db)); 
     }
 
@@ -268,20 +284,131 @@ public:
     }
 
     /* close the database */
-    void close() {
-        ham_status_t st=ham_close(m_db);
+    void close(ham_u32_t flags=0) {
+        ham_status_t st=ham_close(m_db, flags);
         if (st)
             throw error(st);
         m_opened=false;
     }
 
-protected:
+private:
     ham_db_t *m_db;
     bool m_opened;
 
 private:
     /* forbid the use of the copy constructor */
     db(const db &other) { (void)other; }
+};
+
+class cursor
+{
+public:
+    /* constructor */
+    cursor(db *db, ham_u32_t flags=0) {
+        ham_status_t st=ham_cursor_create(db->get_handle(), 0, 
+                        flags, &m_cursor);
+        if (st)
+            throw error(st);
+    }
+
+    /* destructor */
+    ~cursor() {
+        close();
+    }
+
+    /* clone the cursor */
+    cursor *clone() {
+        ham_cursor_t *dest;
+        ham_status_t st=ham_cursor_clone(m_cursor, &dest);
+        if (st)
+            throw error(st);
+        return (new cursor(dest));
+    }
+
+    /* move the cursor */
+    void move(key *key, record *record, ham_u32_t flags=0) {
+        ham_status_t st=ham_cursor_move(m_cursor, key, record, flags);
+        if (st)
+            throw error(st);
+    }
+
+    /* move to the very first element */
+    void begin() {
+        move(0, 0, HAM_CURSOR_FIRST);
+    }
+
+    /* move to the very last element */
+    void end() {
+        move(0, 0, HAM_CURSOR_LAST);
+    }
+
+    /* move to the next element */
+    cursor &operator++() {
+        move(0, 0, HAM_CURSOR_NEXT);
+        return (*this);
+    }
+
+    /* move to the previous element */
+    cursor &operator--() {
+        move(0, 0, HAM_CURSOR_PREVIOUS);
+        return (*this);
+    }
+
+    /* overwrite the current record */
+    void overwrite(record *record, ham_u32_t flags=0) {
+        ham_status_t st=ham_cursor_overwrite(m_cursor, record, flags);
+        if (st)
+            throw error(st);
+    }
+
+    /* find a key */
+    void find(key *akey, ham_u32_t flags=0) {
+        ham_status_t st=ham_cursor_find(m_cursor, key, flags);
+        if (st)
+            throw error(st);
+    }
+
+    /* insert a key */
+    void insert(key *akey, record *rec, ham_u32_t flags=0) {
+        ham_status_t st=ham_cursor_insert(m_cursor, key, rec, flags);
+        if (st)
+            throw error(st);
+    }
+
+    /* erase the current key */
+    void erase(ham_u32_t flags=0) {
+        ham_status_t st=ham_cursor_erase(m_cursor, flags);
+        if (st)
+            throw error(st);
+    }
+
+    /* get number of duplicate keys */
+    ham_u32_t get_duplicate_count(ham_u32_t flags=0) {
+        ham_u32_t c;
+        ham_status_t st=ham_cursor_get_duplicate_count(m_cursor, &c, flags);
+        if (st)
+            throw error(st);
+        return (c);
+    }
+
+    /* close the cursor */
+    void close() {
+        if (!m_cursor)
+            return;
+        ham_status_t st=ham_cursor_close(m_cursor);
+        if (st)
+            throw error(st);
+        m_cursor=0;
+    }
+
+protected:
+    /* constructor */
+    cursor(ham_cursor_t *c) {
+        m_cursor=c;
+    }
+
+private:
+    ham_cursor_t *m_cursor;
 };
 
 }; // namespace ham
