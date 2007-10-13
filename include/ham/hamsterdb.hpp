167be402eeff22762b8ea2bdccef51925cdb52fd
cruppstahl <chris@crupp.de>
2007-10-13 13:53:17 +0000

    c++ API is now nearly complete
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@698 66673985-dd14-0410-9433-caba4d15c716

--- include/ham/hamsterdb.hpp
+++ include/ham/hamsterdb.hpp
@@ -3,13 +3,13 @@
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or 
+ * Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
  * See file COPYING.GPL2 and COPYING.GPL3 for License information.
  *
  *
- * \mainpage hamsterdb C++ header file
+ * \mainpage hamsterdb C++ API
  * \file hamsterdb.hpp
  * \author Christoph Rupp, chris@crupp.de
  * \version 0.4.7
@@ -21,17 +21,19 @@
 /*
  * TODO TODO TODO
  *
- * x syntaxfehler raus
- * x header-kommentar einfuegen!
- *
- * x cursor: begin/end/++/--: sind das die iterator-deklarationen??
- *
- * o sicherstellen dass die std::algorithm-funktionen mit den iteratoren
- *      funktionieren
- *
- * o environments fehlen komplett
- *
- * o 100%ige abdeckung durch unittests!
+ * Questions:
+ *  o parameters as references or pointers?
+ *  o cursors are no iterators - can we change that? -> don't think so
+ *  o make key/record functions virtual? -> can derive classes from 
+ *      keys/records --> would be cool if we could use both interfaces in
+ *      one class! 
+ *  o use exceptions or error codes? -> we actually *could* use return codes,
+ *      since the constructor cannot fail, and we don't have operators
+ *      -> currently i favor exceptions for one reason: they HAVE to be
+ *         handled; the user is forced to check for errors.
+ *  o the assignment operator of class db is ugly; how else can i make
+ *      this work: 
+ *      ham::db db=env.create_db(1);
  *
  * o documentation!!
  * TODO TODO TODO
@@ -49,12 +51,12 @@ public:
     };
 
     /* get the error code */
-    ham_status_t get_errno() {
+    ham_status_t get_errno() const {
         return (m_errno);
     }
 
     /* get an error string */
-    const char *get_string() {
+    const char *get_string() const {
         return (ham_strerror(m_errno));
     }
 
@@ -79,36 +81,36 @@ public:
     /* assignment operator */
     key &operator=(const key &other) {
         m_key=other.m_key;
-        return *this;
+        return (*this);
     }
 
     /* get the key data */
-    void *get_data() { 
-        return m_key.data; 
+    void *get_data() const {
+        return (m_key.data);
     }
 
     /* set the key data */
-    void set_data(void *data) { 
-        m_key.data=data; 
+    void set_data(void *data) {
+        m_key.data=data;
     }
 
     /* get the key size */
-    ham_size_t get_size() { 
-        return m_key.size; 
+    ham_size_t get_size() const {
+        return (m_key.size);
     }
 
     /* set the key size */
-    void set_size(ham_size_t size) { 
+    void set_size(ham_size_t size) {
         m_key.size=size;
     }
 
     /* get the key flags */
-    ham_u32_t get_flags() { 
-        return m_key.flags; 
+    ham_u32_t get_flags() const {
+        return (m_key.flags);
     }
 
     /* set the key flags */
-    void set_flags(ham_u32_t flags) { 
+    void set_flags(ham_u32_t flags) {
         m_key.flags=flags;
     }
 
@@ -135,36 +137,36 @@ public:
     /* assignment operator */
     record &operator=(const record &other) {
         m_rec=other.m_rec;
-        return *this;
+        return (*this);
     }
 
     /* get the record data */
-    void *get_data() { 
-        return m_rec.data; 
+    void *get_data() const {
+        return (m_rec.data);
     }
 
     /* set the record data */
-    void set_data(void *data) { 
-        m_rec.data=data; 
+    void set_data(void *data) {
+        m_rec.data=data;
     }
 
     /* get the record size */
-    ham_size_t get_size() { 
-        return m_rec.size; 
+    ham_size_t get_size() const {
+        return (m_rec.size);
     }
 
     /* set the record size */
-    void set_size(ham_size_t size) { 
+    void set_size(ham_size_t size) {
         m_rec.size=size;
     }
 
     /* get the record flags */
-    ham_u32_t get_flags() { 
-        return m_rec.flags; 
+    ham_u32_t get_flags() const {
+        return (m_rec.flags);
     }
 
     /* set the record flags */
-    void set_flags(ham_u32_t flags) { 
+    void set_flags(ham_u32_t flags) {
         m_rec.flags=flags;
     }
 
@@ -186,26 +188,38 @@ public:
     }
 
     /* get hamsterdb library version */
-    static void get_version(ham_u32_t *major, ham_u32_t *minor, 
+    static void get_version(ham_u32_t *major, ham_u32_t *minor,
                     ham_u32_t *revision) {
         ham_get_version(major, minor, revision);
     }
 
     /* constructor */
-    db() : m_db(0), m_opened(false) { 
+    db() : m_db(0), m_opened(false) {
     }
 
     /* destructor - automatically closes the database, if necessary */
     ~db() {
-        if (m_opened)
-            close();
+        close();
         if (m_db)
             ham_delete(m_db);
         m_db=0;
     }
 
+    /* the assignment operator transfers the ownership of the database
+     * handle */
+    db &operator=(db other) {
+        if (this==&other)
+            return (*this);
+        close();
+        m_db=other.m_db;
+        m_opened=true;
+        other.m_db=0; 
+        other.m_opened=false;
+        return (*this);
+    }
+
     /* create a database */
-    void create(const char *filename, ham_u32_t flags=0, 
+    void create(const char *filename, ham_u32_t flags=0,
             ham_u32_t mode=0644, ham_parameter_t *param=0) {
         ham_status_t st;
         if (!m_db) {
@@ -233,8 +247,8 @@ public:
     }
 
     /* get the last database error */
-    ham_status_t get_error() { 
-        return (ham_get_error(m_db)); 
+    ham_status_t get_error() {
+        return (ham_get_error(m_db));
     }
 
     /* set the prefix comparison function */
@@ -252,9 +266,9 @@ public:
     }
 
     /* find a value */
-    record find(key *k, record *r, ham_u32_t flags=0) {
+    record find(key *k, ham_u32_t flags=0) {
         record r;
-        ham_status_t st=ham_find(m_db, 0, &k->m_key, &r->m_rec, flags);
+        ham_status_t st=ham_find(m_db, 0, &k->m_key, &r.m_rec, flags);
         if (st)
             throw error(st);
         return (r);
@@ -275,7 +289,7 @@ public:
     }
 
     /* flush to disk */
-    void flush(ham_u32_t flags=0) { 
+    void flush(ham_u32_t flags=0) {
         ham_status_t st=ham_flush(m_db, flags);
         if (st)
             throw error(st);
@@ -283,10 +297,16 @@ public:
 
     /* close the database */
     void close(ham_u32_t flags=0) {
+        if (!m_opened)
+            return;
         ham_status_t st=ham_close(m_db, flags);
         if (st)
             throw error(st);
         m_opened=false;
+        st=ham_delete(m_db);
+        if (st)
+            throw error(st);
+        m_db=0;
     }
 
     /* get the database handle */
@@ -294,24 +314,25 @@ public:
         return (m_db);
     }
 
+protected:
+    friend class env;
+
+    /* constructor */
+    db(ham_db_t *db) : m_db(db), m_opened(true) {
+    }
+
 private:
     ham_db_t *m_db;
     bool m_opened;
-
-private:
-    /* forbid the use of the copy constructor */
-    db(const db &other) { (void)other; }
 };
 
 class cursor
 {
 public:
     /* constructor */
-    cursor(db *db, ham_u32_t flags=0) {
-        ham_status_t st=ham_cursor_create(db->get_handle(), 0, 
-                        flags, &m_cursor);
-        if (st)
-            throw error(st);
+    cursor(db *db=0, ham_u32_t flags=0)
+    :   m_cursor(0) {
+        create(db, flags);
     }
 
     /* destructor */
@@ -319,43 +340,51 @@ public:
         close();
     }
 
+    /* create a cursor */
+    void create(db *db, ham_u32_t flags=0) {
+        if (db) {
+            ham_status_t st=ham_cursor_create(db->get_handle(), 0,
+                            flags, &m_cursor);
+            if (st)
+                throw error(st);
+        }
+    }
+
     /* clone the cursor */
-    cursor *clone() {
+    cursor clone() {
         ham_cursor_t *dest;
         ham_status_t st=ham_cursor_clone(m_cursor, &dest);
         if (st)
             throw error(st);
-        return (new cursor(dest));
+        return (cursor(dest));
     }
 
     /* move the cursor */
     void move(key *k, record *rec, ham_u32_t flags=0) {
-        ham_status_t st=ham_cursor_move(m_cursor, &k->m_key, 
+        ham_status_t st=ham_cursor_move(m_cursor, &k->m_key,
                         &rec->m_rec, flags);
         if (st)
             throw error(st);
     }
 
     /* move to the very first element */
-    void begin() {
-        move(0, 0, HAM_CURSOR_FIRST);
+    void begin(key *k=0, record *r=0) {
+        move(k, r, HAM_CURSOR_FIRST);
     }
 
     /* move to the very last element */
-    void end() {
-        move(0, 0, HAM_CURSOR_LAST);
+    void end(key *k=0, record *r=0) {
+        move(k, r, HAM_CURSOR_LAST);
     }
 
     /* move to the next element */
-    cursor &operator++() {
-        move(0, 0, HAM_CURSOR_NEXT);
-        return (*this);
+    void next(key *k=0, record *r=0) {
+        move(k, r, HAM_CURSOR_NEXT);
     }
 
     /* move to the previous element */
-    cursor &operator--() {
-        move(0, 0, HAM_CURSOR_PREVIOUS);
-        return (*this);
+    void previous(key *k=0, record *r=0) {
+        move(k, r, HAM_CURSOR_PREVIOUS);
     }
 
     /* overwrite the current record */
@@ -374,7 +403,7 @@ public:
 
     /* insert a key */
     void insert(key *k, record *r, ham_u32_t flags=0) {
-        ham_status_t st=ham_cursor_insert(m_cursor, &k->m_key, 
+        ham_status_t st=ham_cursor_insert(m_cursor, &k->m_key,
                         &r->m_rec, flags);
         if (st)
             throw error(st);
@@ -414,6 +443,118 @@ protected:
 
 private:
     ham_cursor_t *m_cursor;
+};
+
+class env
+{
+public:
+    /* constructor */
+    env() : m_env(0), m_opened(false) {
+    }
+
+    /* destructor - automatically closes the environment, if necessary */
+    ~env() {
+        close();
+        if (m_env)
+            ham_env_delete(m_env);
+        m_env=0;
+    }
+
+    /* create an environment */
+    void create(const char *filename, ham_u32_t flags=0,
+            ham_u32_t mode=0644, ham_parameter_t *param=0) {
+        ham_status_t st;
+        if (!m_env) {
+            st=ham_env_new(&m_env);
+            if (st)
+                throw error(st);
+        }
+        if ((st=ham_env_create_ex(m_env, filename, flags, mode, param)))
+            throw error(st);
+        m_opened=true;
+    }
+
+    /* open an environment */
+    void open(const char *filename, ham_u32_t flags=0,
+            ham_parameter_t *param=0) {
+        ham_status_t st;
+        if (!m_env) {
+            st=ham_env_new(&m_env);
+            if (st)
+                throw error(st);
+        }
+        if ((st=ham_env_open_ex(m_env, filename, flags, param)))
+            throw error(st);
+        m_opened=true;
+    }
+
+    /* create a new database in the environment */
+    db create_db(ham_u16_t name, ham_u32_t flags=0, ham_parameter_t *param=0) {
+        ham_status_t st;
+        ham_db_t *dbh;
+
+        st=ham_new(&dbh);
+        if (st)
+            throw error(st);
+        st=ham_env_create_db(m_env, dbh, name, flags, param);
+        if (st) {
+            ham_delete(dbh);
+            throw error(st);
+        }
+
+        ham::db d(dbh);
+        return (d);
+    }
+
+    /* open an existing database in the environment */
+    db open_db(ham_u16_t name, ham_u32_t flags=0, ham_parameter_t *param=0) {
+        ham_status_t st;
+        ham_db_t *dbh;
+
+        st=ham_new(&dbh);
+        if (st)
+            throw error(st);
+        st=ham_env_open_db(m_env, dbh, name, flags, param);
+        if (st) {
+            ham_delete(dbh);
+            throw error(st);
+        }
+
+        ham::db d(dbh);
+        return (d);
+    }
+
+    /* rename a database */
+    void rename_db(ham_u16_t oldname, ham_u16_t newname, ham_u32_t flags=0) {
+        ham_status_t st=ham_env_rename_db(m_env, oldname, newname, flags);
+        if (st)
+            throw error(st);
+    }
+
+    /* delete a database from the environment */
+    void erase_db(ham_u16_t name, ham_u32_t flags=0) {
+        ham_status_t st=ham_env_erase_db(m_env, name, flags);
+        if (st)
+            throw error(st);
+    }
+
+    /* closes the environment */
+    void close(ham_u32_t flags=0) {
+        if (!m_opened)
+            return;
+        ham_status_t st=ham_env_close(m_env, flags);
+        if (st)
+            throw error(st);
+        m_opened=false;
+        st=ham_env_delete(m_env);
+        if (st)
+            throw error(st);
+        m_env=0;
+    }
+
+private:
+    ham_env_t *m_env;
+    bool m_opened;
 };
 
 }; // namespace ham
--- unittests/cppapi.cpp
+++ unittests/cppapi.cpp
@@ -42,8 +42,13 @@ my_prefix_compare_func(const ham_u8_t *lhs, ham_size_t lhs_length,
 class CppApiTest : public CppUnit::TestFixture
 {
     CPPUNIT_TEST_SUITE(CppApiTest);
+    CPPUNIT_TEST      (keyTest);
+    CPPUNIT_TEST      (recordTest);
+    CPPUNIT_TEST      (compareTest);
     CPPUNIT_TEST      (createOpenCloseDbTest);
     CPPUNIT_TEST      (insertFindEraseTest);
+    CPPUNIT_TEST      (cursorTest);
+    CPPUNIT_TEST      (envTest);
     CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -53,6 +58,79 @@ public:
     
     void tearDown() 
     { 
+    }
+
+    void keyTest(void)
+    {
+        void *p=(void *)"123";
+        void *q=(void *)"234";
+        ham::key k1, k2(p, 4, HAM_KEY_USER_ALLOC);
+
+        CPPUNIT_ASSERT_EQUAL((void *)0, k1.get_data());
+        CPPUNIT_ASSERT_EQUAL((ham_size_t)0, k1.get_size());
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)0, k1.get_flags());
+
+        CPPUNIT_ASSERT_EQUAL(p, k2.get_data());
+        CPPUNIT_ASSERT_EQUAL((ham_size_t)4, k2.get_size());
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)HAM_KEY_USER_ALLOC, k2.get_flags());
+
+        k1=k2;
+        CPPUNIT_ASSERT_EQUAL(p, k1.get_data());
+        CPPUNIT_ASSERT_EQUAL((ham_size_t)4, k1.get_size());
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)HAM_KEY_USER_ALLOC, k1.get_flags());
+
+        ham::key k3(k1);
+        CPPUNIT_ASSERT_EQUAL(p, k3.get_data());
+        CPPUNIT_ASSERT_EQUAL((ham_size_t)4, k3.get_size());
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)HAM_KEY_USER_ALLOC, k3.get_flags());
+
+        k1.set_data(q);
+        k1.set_size(2);
+        k1.set_flags(0);
+        CPPUNIT_ASSERT_EQUAL(q, k1.get_data());
+        CPPUNIT_ASSERT_EQUAL((ham_size_t)2, k1.get_size());
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)0, k1.get_flags());
+    }
+
+    void recordTest(void)
+    {
+        void *p=(void *)"123";
+        void *q=(void *)"234";
+        ham::record r1, r2(p, 4, HAM_RECORD_USER_ALLOC);
+
+        CPPUNIT_ASSERT_EQUAL((void *)0, r1.get_data());
+        CPPUNIT_ASSERT_EQUAL((ham_size_t)0, r1.get_size());
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)0, r1.get_flags());
+
+        CPPUNIT_ASSERT_EQUAL(p, r2.get_data());
+        CPPUNIT_ASSERT_EQUAL((ham_size_t)4, r2.get_size());
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)HAM_RECORD_USER_ALLOC, r2.get_flags());
+
+        r1=r2;
+        CPPUNIT_ASSERT_EQUAL(p, r1.get_data());
+        CPPUNIT_ASSERT_EQUAL((ham_size_t)4, r1.get_size());
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)HAM_RECORD_USER_ALLOC, r1.get_flags());
+
+        ham::record r3(r1);
+        CPPUNIT_ASSERT_EQUAL(p, r3.get_data());
+        CPPUNIT_ASSERT_EQUAL((ham_size_t)4, r3.get_size());
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)HAM_RECORD_USER_ALLOC, r3.get_flags());
+
+        r1.set_data(q);
+        r1.set_size(2);
+        r1.set_flags(0);
+        CPPUNIT_ASSERT_EQUAL(q, r1.get_data());
+        CPPUNIT_ASSERT_EQUAL((ham_size_t)2, r1.get_size());
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)0, r1.get_flags());
+    }
+
+    void compareTest(void)
+    {
+        ham::db db;
+        db.create(".test");
+        db.set_compare_func(my_compare_func);
+        db.set_prefix_compare_func(my_prefix_compare_func);
+        db.close();
     }
 
     void createOpenCloseDbTest(void)
@@ -74,27 +152,89 @@ public:
         r.set_data((void *)"12345");
         r.set_size(6);
 
-        ham_key_t ktmp1=k;
-        ham_key_t ktmp2(k);
-        ham_record_t rtmp1=r;
-        ham_record_t rtmp2(r);
-
         db.create(".test");
         db.insert(&k, &r);
-        db.find(&k, &out);
+        out=db.find(&k);
         CPPUNIT_ASSERT_EQUAL(r.get_size(), out.get_size());
         CPPUNIT_ASSERT_EQUAL(0,
                         ::memcmp(r.get_data(), out.get_data(), out.get_size()));
         db.erase(&k);
         try {
-            db.find(&k, &out);
+            out=db.find(&k);
         }
-        catch(error &e) {
+        catch(ham::error &e) {
             CPPUNIT_ASSERT_EQUAL(HAM_KEY_NOT_FOUND, e.get_errno());
         }
 
         db.close();
+        db.close();
+        db.close();
         db.open(".test");
+    }
+
+    void cursorTest(void)
+    {
+        ham::db db;
+        ham::key k((void *)"12345", 5), k2;
+        ham::record r((void *)"12345", 5), r2;
+
+        db.create(".test");
+        ham::cursor c(&db);
+
+        db.insert(&k, &r);
+        ham::cursor clone=c.clone();
+
+        c.begin(&k2, &r2);
+        CPPUNIT_ASSERT_EQUAL(k.get_size(), k2.get_size());
+        CPPUNIT_ASSERT_EQUAL(r.get_size(), r2.get_size());
+
+        c.end(&k2, &r2);
+        CPPUNIT_ASSERT_EQUAL(k.get_size(), k2.get_size());
+        CPPUNIT_ASSERT_EQUAL(r.get_size(), r2.get_size());
+
+        try {
+            c.next();
+        }
+        catch (ham::error &e) {
+            CPPUNIT_ASSERT_EQUAL(e.get_errno(), HAM_KEY_NOT_FOUND);
+        }
+
+        try {
+            c.previous();
+        }
+        catch (ham::error &e) {
+            CPPUNIT_ASSERT_EQUAL(e.get_errno(), HAM_KEY_NOT_FOUND);
+        }
+
+        c.find(&k);
+        CPPUNIT_ASSERT_EQUAL((ham_u32_t)1, c.get_duplicate_count());
+
+        c.erase();
+    }
+
+    void envTest(void)
+    {
+        ham::env env;
+
+        env.create(".test");
+        env.close();
+        env.close();
+        env.close();
+        env.open(".test");
+
+        ham::db db1=env.create_db(1);
+        db1.close();
+        db1=env.open_db(1);
+        env.rename_db(1, 2);
+
+        try {
+            env.erase_db(2);
+        }
+        catch (ham::error &e) {
+            CPPUNIT_ASSERT_EQUAL(HAM_DATABASE_ALREADY_OPEN, e.get_errno());
+        }
+        db1.close();
+        env.erase_db(2);
     }
 };
 
