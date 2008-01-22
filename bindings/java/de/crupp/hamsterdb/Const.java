6737eedee5a7f3f8e7df2ba12f26fb7273e4e1a8
cruppstahl <chris@crupp.de>
2008-01-22 20:55:27 +0000

    added header comments/copyright note
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@959 66673985-dd14-0410-9433-caba4d15c716

--- bindings/java/de/crupp/hamsterdb/Const.java
+++ bindings/java/de/crupp/hamsterdb/Const.java
@@ -1,3 +1,15 @@
+/**
+ * Copyright (C) 2005-2008 Christoph Rupp (chris@crupp.de).
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or 
+ * (at your option) any later version.
+ *
+ * See file COPYING.GPL2 and COPYING.GPL3 for License information.
+ *
+ */
+
 package de.crupp.hamsterdb;
 
 public class Const {
