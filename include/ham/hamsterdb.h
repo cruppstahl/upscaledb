30ede6870649c804f8c3b6f246e7ddef48918a96
cruppstahl <chris@crupp.de>
2009-02-23 08:27:58 +0000

    release 1.0.7
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@1198 66673985-dd14-0410-9433-caba4d15c716

--- include/ham/hamsterdb.h
+++ include/ham/hamsterdb.h
@@ -12,7 +12,7 @@
  * \mainpage hamsterdb embedded database
  * \brief Include file for hamsterdb
  * \author Christoph Rupp, chris@crupp.de
- * \version 1.0.6
+ * \version 1.0.7
  */
 
 #ifndef HAM_HAMSTERDB_H__
