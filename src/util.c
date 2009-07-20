f061c671ab6d91eb7c7c7792b1cdbdbce6373eda
cruppstahl <chris@crupp.de>
2009-07-20 19:00:49 +0000

    vsnprintf exists on win32, too
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@1244 66673985-dd14-0410-9433-caba4d15c716

--- src/util.c
+++ src/util.c
@@ -29,8 +29,10 @@ extern int vsnprintf(char *str, size_t size, const char *format, va_list ap);
 int
 util_vsnprintf(char *str, size_t size, const char *format, va_list ap)
 {
-#if HAM_OS_POSIX
+#if defined(HAM_OS_POSIX)
     return vsnprintf(str, size, format, ap);
+#elif defined(HAM_OS_WIN32)
+    return _vsnprintf(str, size, format, ap);
 #else
     (void)size;
     return vsprintf(str, format, ap);
