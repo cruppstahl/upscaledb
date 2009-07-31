edd548684d7832fb50a2a3c4152779645a58c505
cruppstahl <chris@crupp.de>
2009-07-31 09:19:27 +0000

    db_get_error shouldn't throw
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@1285 66673985-dd14-0410-9433-caba4d15c716

--- include/ham/hamsterdb.hpp
+++ include/ham/hamsterdb.hpp
@@ -322,9 +322,8 @@ public:
 
     /** Returns the last Database error. */
     ham_status_t get_error() {
-        if (!m_db) {
-			throw error(HAM_NOT_INITIALIZED);
-        }
+        if (!m_db)
+			return (HAM_NOT_INITIALIZED);
         return (ham_get_error(m_db));
     }
 
