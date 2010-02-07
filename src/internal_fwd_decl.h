2c519759361c120f810e2d0cfc4a8e0b0016bb11
cruppstahl <chris@crupp.de>
2010-02-07 12:20:28 +0000

    whitespace fixes
    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@1483 66673985-dd14-0410-9433-caba4d15c716

--- src/internal_fwd_decl.h
+++ src/internal_fwd_decl.h
@@ -12,10 +12,10 @@
 /**
  * @brief provides forward declarations of internally used types
  *
- * This header file provides these forward declarations to prevent several cyclic
- * dependencies; in particular, the @ref ham_page_t is a type used throughout, but
- * a @ref ham_page_t contains several other types, which again reference 
- * @ref ham_page_t pointers either directly or indirectly.
+ * This header file provides these forward declarations to prevent several 
+ * cyclic dependencies; in particular, the @ref ham_page_t is a type used 
+ * throughout, but a @ref ham_page_t contains several other types, which 
+ * again reference @ref ham_page_t pointers either directly or indirectly.
  *
  * To solve this self-referential issue once and for all, all major hamster 
  * internal types are forward declared here; when the code requires the actual 
@@ -23,8 +23,8 @@
  * wishes.
  *
  * @remark This way of solving the cyclic dependency conundrum has the added
- *         benefit of having a single, well known spot where the actual
- *         'typedef' lines reside, so there's zero risk of 'double defined types'.
+ *     benefit of having a single, well known spot where the actual
+ *     'typedef' lines reside, so there's zero risk of 'double defined types'.
  */
 
 #ifndef HAM_FWD_DECL_CMN_TYPES_H__
@@ -46,9 +46,6 @@ typedef struct ham_page_t ham_page_t;
 struct mem_allocator_t;
 typedef struct mem_allocator_t mem_allocator_t;
 
-//struct ham_cursor_t;
-//typedef struct ham_cursor_t ham_cursor_t;
-
 struct ham_backend_t;
 typedef struct ham_backend_t ham_backend_t;
 
@@ -64,7 +61,6 @@ typedef struct extkey_t extkey_t;
 struct extkey_cache_t;
 typedef struct extkey_cache_t extkey_cache_t;
 
-
 struct freelist_entry_t;
 typedef struct freelist_entry_t freelist_entry_t;
 
@@ -74,13 +70,11 @@ typedef struct freelist_cache_t freelist_cache_t;
 struct freelist_hints_t;
 typedef struct freelist_hints_t freelist_hints_t;
 
-
 struct runtime_statistics_pagedata_t;
 typedef struct runtime_statistics_pagedata_t runtime_statistics_pagedata_t;
 
 struct freelist_global_hints_t;
 typedef struct freelist_global_hints_t freelist_global_hints_t;
-
 
 struct find_hints_t;
 typedef struct find_hints_t find_hints_t;
@@ -107,8 +101,6 @@ struct int_key_t;
 typedef struct int_key_t int_key_t;
 
 #include "packstop.h"
-
-
 
 
 #ifdef __cplusplus
