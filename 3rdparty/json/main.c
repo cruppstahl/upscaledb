5318a9c303e9037c9e3df1a9faefcfcd3424d59c
cruppstahl <chris@crupp.de>
2010-04-20 18:42:27 +0000

    git-svn-id: svn+ssh://hamsterdb.com/home/chris/repos/hamsterdb/trunk@1625 66673985-dd14-0410-9433-caba4d15c716

---
+++ 3rdparty/json/main.c
@@ -0,0 +1,143 @@
+/* main.c */
+
+/*
+    This program demonstrates a simple application of JSON_parser. It reads
+    a JSON text from STDIN, producing an error message if the text is rejected.
+
+        % JSON_parser <test/pass1.json
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+
+#include "JSON_parser.h"
+
+static size_t s_Level = 0;
+
+static const char* s_pIndention = "  ";
+
+static int s_IsKey = 0;
+
+static void print_indention()
+{
+    size_t i;
+    
+    for (i = 0; i < s_Level; ++i) {
+        printf(s_pIndention);
+    }
+}
+ 
+
+static int print(void* ctx, int type, const JSON_value* value)
+{
+    switch(type) {
+    case JSON_T_ARRAY_BEGIN:    
+        if (!s_IsKey) print_indention();
+        s_IsKey = 0;
+        printf("[\n");
+        ++s_Level;
+        break;
+    case JSON_T_ARRAY_END:
+        assert(!s_IsKey);
+        if (s_Level > 0) --s_Level;
+        print_indention();
+        printf("]\n");
+        break;
+   case JSON_T_OBJECT_BEGIN:
+       if (!s_IsKey) print_indention();
+       s_IsKey = 0;
+       printf("{\n");
+        ++s_Level;
+        break;
+    case JSON_T_OBJECT_END:
+        assert(!s_IsKey);
+        if (s_Level > 0) --s_Level;
+        print_indention();
+        printf("}\n");
+        break;
+    case JSON_T_INTEGER:
+        if (!s_IsKey) print_indention();
+        s_IsKey = 0;
+        printf("integer: "JSON_PARSER_INTEGER_SPRINTF_TOKEN"\n", value->vu.integer_value);
+        break;
+    case JSON_T_FLOAT:
+        if (!s_IsKey) print_indention();
+        s_IsKey = 0;
+        printf("float: %s\n", value->vu.str.value); /* We wanted stringified floats */
+        break;
+    case JSON_T_NULL:
+        if (!s_IsKey) print_indention();
+        s_IsKey = 0;
+        printf("null\n");
+        break;
+    case JSON_T_TRUE:
+        if (!s_IsKey) print_indention();
+        s_IsKey = 0;
+        printf("true\n");
+        break;
+    case JSON_T_FALSE:
+        if (!s_IsKey) print_indention();
+        s_IsKey = 0;
+        printf("false\n");
+        break;
+    case JSON_T_KEY:
+        s_IsKey = 1;
+        print_indention();
+        printf("key = '%s', value = ", value->vu.str.value);
+        break;   
+    case JSON_T_STRING:
+        if (!s_IsKey) print_indention();
+        s_IsKey = 0;
+        printf("string: '%s'\n", value->vu.str.value);
+        break;
+    default:
+        assert(0);
+        break;
+    }
+    
+    return 1;
+}
+
+int main(int argc, char* argv[]) {
+    int count = 0;
+/*
+    Read STDIN. Exit with a message if the input is not well-formed JSON text.
+
+    jc will contain a JSON_parser with a maximum depth of 20.
+*/
+
+    JSON_config config;
+
+    struct JSON_parser_struct* jc = NULL;
+    
+    init_JSON_config(&config);
+    
+    config.depth                  = 20;
+    config.callback               = &print;
+    config.allow_comments         = 1;
+    config.handle_floats_manually = 1;
+    
+    jc = new_JSON_parser(&config);
+    
+    FILE* input = stdin;
+    for (; input ; ++count) {
+        int next_char = fgetc(input);
+        if (next_char <= 0) {
+            break;
+        }
+        if (!JSON_parser_char(jc, next_char)) {
+            delete_JSON_parser(jc);
+            fprintf(stderr, "JSON_parser_char: syntax error, byte %d\n", count);
+            return 1;
+        }
+    }
+    if (!JSON_parser_done(jc)) {
+        delete_JSON_parser(jc);
+        fprintf(stderr, "JSON_parser_end: syntax error\n");
+        return 1;
+    }
+    
+    return 0;
+}
