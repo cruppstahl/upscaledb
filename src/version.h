fd29c88c6a1d5e0ef7f946caeca72d5154074567
Christoph Rupp <chris@crupp.de>
2010-10-31 13:00:39 +0100

    release 1.1.7

--- src/version.h
+++ src/version.h
@@ -33,8 +33,8 @@ extern "C" {
  */
 #define HAM_VERSION_MAJ 1
 #define HAM_VERSION_MIN 1
-#define HAM_VERSION_REV 6
-#define HAM_VERSION_STR "1.1.6"
+#define HAM_VERSION_REV 7
+#define HAM_VERSION_STR "1.1.7"
 
 
 #ifdef __cplusplus
