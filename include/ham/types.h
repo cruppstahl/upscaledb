dc45f6d8a7e5da0bf31e25cd67166b6fda230a99
Christoph Rupp <chris@crupp.de>
2013-01-05 14:54:45 +0100

    revert usage of stdint.h because it's not available in visual studio

--- include/ham/types.h
+++ include/ham/types.h
@@ -104,16 +104,44 @@ extern "C" {
 #  define HAM_CALLCONV
 #endif
 
-#include <stdint.h>
+/**
+ * typedefs for 32bit operating systems
+ */
+#ifdef HAM_32BIT
+#  ifdef _MSC_VER
+typedef signed __int64     ham_s64_t;
+typedef unsigned __int64   ham_u64_t;
+#  else
+typedef signed long long   ham_s64_t;
+typedef unsigned long long ham_u64_t;
+#  endif
+typedef signed int         ham_s32_t;
+typedef unsigned int       ham_u32_t;
+typedef signed short       ham_s16_t;
+typedef unsigned short     ham_u16_t;
+typedef signed char        ham_s8_t;
+typedef unsigned char      ham_u8_t;
+#endif
 
-typedef int64_t            ham_s64_t;
-typedef uint64_t           ham_u64_t;
-typedef int32_t            ham_s32_t;
-typedef uint32_t           ham_u32_t;
-typedef int16_t            ham_s16_t;
-typedef uint16_t           ham_u16_t;
-typedef int8_t             ham_s8_t;
-typedef uint8_t            ham_u8_t;
+/**
+ * typedefs for 64bit operating systems; on Win64,
+ * longs do not always have 64bit!
+ */
+#ifdef HAM_64BIT
+#  ifdef _MSC_VER
+typedef signed __int64     ham_s64_t;
+typedef unsigned __int64   ham_u64_t;
+#  else
+typedef signed long        ham_s64_t;
+typedef unsigned long      ham_u64_t;
+#  endif
+typedef signed int         ham_s32_t;
+typedef unsigned int       ham_u32_t;
+typedef signed short       ham_s16_t;
+typedef unsigned short     ham_u16_t;
+typedef signed char        ham_s8_t;
+typedef unsigned char      ham_u8_t;
+#endif
 
 /*
  * Undefine macros to avoid macro redefinitions
