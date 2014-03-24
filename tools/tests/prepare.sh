05160837f5412c6386ddcb53f2708fb61311a04a
Christoph Rupp <chris@crupp.de>
2014-03-24 23:23:53 +0100

    more minor updates for 2.1.7

--- tools/tests/prepare.sh
+++ tools/tests/prepare.sh
@@ -9,6 +9,7 @@ mv test.db env1.db
 ../../samples/env3
 mv test.db env3.db
 
-echo now run ./ham_bench ../../../hamsterdb-tests/testfiles/1/ext_020.tst
+echo now run ../ham_bench/ham_bench ../../../hamsterdb-tests/testfiles/1/ext_020.tst
 echo and copy test-ham.db to extkeys.db
+echo then run ham_info/prepare.sh
 
