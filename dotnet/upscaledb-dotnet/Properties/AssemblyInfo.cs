31be92d295efb00af695e619654112a526ce6d15
Christoph Rupp <chris@crupp.de>
2016-02-25 18:31:32 +0100

    Copyright is now 2016 for all files

--- dotnet/upscaledb-dotnet/Properties/AssemblyInfo.cs
+++ dotnet/upscaledb-dotnet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Christoph Rupp")]
 [assembly: AssemblyProduct("upscaledb-dotnet")]
-[assembly: AssemblyCopyright("Copyright © 2015 Christoph Rupp")]
+[assembly: AssemblyCopyright("Copyright © 2016 Christoph Rupp")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
