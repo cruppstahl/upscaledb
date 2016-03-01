74fcbcb30d8fd1d95ac8c672831f4958723045d8
Christoph Rupp <chris@crupp.de>
2016-03-01 20:46:20 +0100

    Added missing files

---
+++ src/3cache/cache_state.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2005-2016 Christoph Rupp (chris@crupp.de).
+ * All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * See the file COPYING for License information.
+ */
+
+#ifndef UPS_CACHE_STATE_H
+#define UPS_CACHE_STATE_H
+
+#include "0root/root.h"
+
+#include <vector>
+
+#include "ups/types.h"
+
+// Always verify that a file of level N does not include headers > N!
+#include "2page/page.h"
+#include "2page/page_collection.h"
+#include "2config/env_config.h"
+
+#ifndef UPS_ROOT_H
+#  error "root.h was not included"
+#endif
+
+namespace upscaledb {
+
+struct CacheState
+{
+  typedef PageCollection<Page::kListBucket> CacheLine;
+
+  enum {
+    // The number of buckets should be a prime number or similar, as it
+    // is used in a MODULO hash scheme
+    kBucketSize = 10317,
+  };
+
+  CacheState(const EnvConfig &config)
+    : capacity_bytes(isset(config.flags, UPS_CACHE_UNLIMITED)
+                            ? std::numeric_limits<uint64_t>::max()
+                            : config.cache_size_bytes),
+      page_size_bytes(config.page_size_bytes), alloc_elements(0),
+      buckets(kBucketSize), cache_hits(0), cache_misses(0) {
+    assert(capacity_bytes > 0);
+  }
+
+  // the capacity (in bytes)
+  uint64_t capacity_bytes;
+
+  // the current page size (in bytes)
+  uint64_t page_size_bytes;
+
+  // the current number of cached elements that were allocated (and not
+  // mapped)
+  size_t alloc_elements;
+
+  // linked list of ALL cached pages
+  PageCollection<Page::kListCache> totallist;
+
+  // The hash table buckets - each is a linked list of Page pointers
+  std::vector<CacheLine> buckets;
+
+  // counts the cache hits
+  uint64_t cache_hits;
+
+  // counts the cache misses
+  uint64_t cache_misses;
+};
+
+} // namespace upscaledb
+
+#endif /* UPS_CACHE_STATE_H */
---
+++ src/3journal/journal_state.h
@@ -0,0 +1,97 @@
+/*
+ * Copyright (C) 2005-2016 Christoph Rupp (chris@crupp.de).
+ * All Rights Reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * See the file COPYING for License information.
+ */
+
+#ifndef UPS_JOURNAL_STATE_H
+#define UPS_JOURNAL_STATE_H
+
+#include "0root/root.h"
+
+#include <vector>
+#include <string>
+
+#include "ups/types.h" // for metrics
+
+#include "1base/dynamic_array.h"
+#include "1base/scoped_ptr.h"
+#include "1os/file.h"
+#include "2page/page_collection.h"
+#include "2compressor/compressor.h"
+
+// Always verify that a file of level N does not include headers > N!
+
+#ifndef UPS_ROOT_H
+#  error "root.h was not included"
+#endif
+
+namespace upscaledb {
+
+class Database;
+class LocalEnvironment;
+
+struct JournalState
+{
+  JournalState(LocalEnvironment *env_);
+
+  // References the Environment this journal file is for
+  LocalEnvironment *env;
+
+  // The index of the file descriptor we are currently writing to (0 or 1)
+  uint32_t current_fd;
+
+  // The two file descriptors
+  File files[2];
+
+  // Buffers for writing data to the files
+  ByteArray buffer[2];
+
+  // For counting all open transactions in the files
+  uint64_t open_txn[2];
+
+  // For counting all closed transactions in the files
+  // This needs to be atomic since it's updated from the worker thread
+  boost::atomic<uint64_t> closed_txn[2];
+
+  // The lsn of the previous checkpoint
+  uint64_t last_cp_lsn;
+
+  // When having more than these Transactions in one file, we
+  // swap the files
+  uint64_t threshold;
+
+  // Set to false to disable logging; used during recovery
+  bool disable_logging;
+
+  // Counting the flushed bytes (for ups_env_get_metrics)
+  uint64_t count_bytes_flushed;
+
+  // Counting the bytes before compression (for ups_env_get_metrics)
+  uint64_t count_bytes_before_compression;
+
+  // Counting the bytes after compression (for ups_env_get_metrics)
+  uint64_t count_bytes_after_compression;
+
+  // A map of all opened Databases
+  typedef std::map<uint16_t, Database *> DatabaseMap;
+  DatabaseMap database_map;
+
+  // The compressor; can be null
+  ScopedPtr<Compressor> compressor;
+};
+
+} // namespace upscaledb
+
+#endif /* UPS_JOURNAL_STATE_H */
